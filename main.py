b391ae40bc3aaa000c170ea8d8241f309ba404dc
energy <energyconsulting?morocco@gmail.com>
2025-11-27 23:08:15 +0100

    Add .gitignore and update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime, timedelta
 
-
+from typing import Any, Dict, List, Union
 from fastapi import FastAPI, Request, Depends, Form, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
